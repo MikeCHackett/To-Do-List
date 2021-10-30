02cd9f9855f1288b92b60ed581ca621dcfe7a4b1
Michael Hackett <87140276+MikeCHackett@users.noreply.github.com>
2021-10-30 18:30:16 -0400

    Delete todo.js

--- todo.js
+++
@@ -1,9 +0,0 @@
-function Todo({todo,index,remove}){
-    function handle(){
-      console.log('Ping:',index);
-      remove(index);
-    }
-    return <div className="todo" onClick={handle} >{todo.text} <button> √ </button> 
-    
-    </div>;
-  }
